e82884fbd1c2e8f4a2fbc0e4981aa99a24b7ac22
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-08-02 11:34:08 +0300

    Innitial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservoirController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['prefix' => 'authors'], function(){
+Route::get('', [AuthorController::class, 'index'])->name('author.index');
+Route::get('create', [AuthorController::class, 'create'])->name('author.create'); 
+Route::post('store', [AuthorController::class, 'store'])->name('author.store'); 
+Route::get('edit/{author}', [AuthorController::class, 'edit'])->name('author.edit'); 
+Route::post('update/{author}', [AuthorController::class, 'update'])->name('author.update'); 
+Route::post('delete/{author}', [AuthorController::class, 'destroy'])->name('author.destroy'); 
+Route::get('show/{author}', [AuthorController::class, 'show'])->name('author.show');
+});
